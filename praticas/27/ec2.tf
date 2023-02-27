e787f48e562e81d2cc196031be311537a2ae0317
Alex <alex.rodrigues23997@gmail.com>
2023-02-27 02:42:14 -0300

    Updating notes

--- praticas/27/ec2.tf
+++ praticas/27/ec2.tf
@@ -2,9 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0b0d54b52c62864d6"
   instance_type = "t2.micro"
 
-  for_each = toset(var.ec2Names)
-
   tags = {
-    "Name" = each.value
+    "Name" = "tf"
   }
 }
